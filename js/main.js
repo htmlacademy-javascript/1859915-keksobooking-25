cfa3059b540466984492f77a2879f08617325dce
Iriny <stepanova1rinas@yandex.ru>
2022-02-18 23:29:39 +0300

    Функции для выюора случайных чисел в диапазоне

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*  if (min > max) {
+    min = Math.ceil(max);
+    max = Math.floor(min);
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+  }
+*/
+
+//Функция для вывода случайного целого числа из диапазона
+function getRandomInt (min, max) {
+
+  if ((min >= max) || (min < 0)) {
+    return 'Диапазон задан неверно';
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomInt(3,5));
+
+//Функция для вывода случайной координаты из диапазона (задано количество знаков после запятой)
+function getRandomCoordinate (min, max, decimals) {
+  if ((min >= max) || (min < 0)) {
+    return 'Диапазон для координаты задан неверно';
+  }
+
+  let coordinate = Math.random() * (max - min + 1) + min;
+  return coordinate.toFixed(decimals);
+}
+
+
+function getRandomCoordinate (min, max, decimals) {
+  if ((min >= max) || (min < 0)) {
+    return 'Диапазон для координаты задан неверно';
+  }
+
+  let coordinate;
+
+  if ((max - min) < 1) {
+    coordinate = Math.random() * (max - min) + min;
+  } else {
+    coordinate = Math.random() * (max - min + 1) + min;
+  }
+  return coordinate.toFixed(decimals);
+}
