0410d8bcc139a603e0b87abe0ff56c83d176d14e
Iriny <stepanova1rinas@yandex.ru>
2022-03-01 23:19:12 +0300

    Генерация данных 1 часть

--- js/main.js
+++ js/main.js
@@ -1,52 +1,110 @@
-/*  if (min > max) {
-    min = Math.ceil(max);
-    max = Math.floor(min);
-  } else {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  }
-*/
 
-//Функция для вывода случайного целого числа из диапазона
-function getRandomInt (min, max) {
+/**
+ * Функция для вывода случайного целого числа из диапазона
+ * @param {Number} min — минимальное число
+ * @param {Number} max — максимальное число
+ * @return {integer} — случайное число
+ */
 
-  if ((min >= max) || (min < 0)) {
-    return 'Диапазон задан неверно';
-  }
+ function getRandomPositiveInteger (a, b) {
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (max - min + 1) + min;
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(result);
 }
 
-getRandomInt(3,5);
+getRandomPositiveInteger(3,5);
 
-//Функция для вывода случайной координаты из диапазона (задано количество знаков после запятой)
-function getRandomCoordinate (min, max, decimals) {
-  if ((min >= max) || (min < 0)) {
-    return 'Диапазон для координаты задан неверно';
+/**
+ * Функция для вывода случайного числа с плавающей запятой из диапазона
+ * @param {Number} min — минимальное число
+ * @param {Number} max — максимальное число
+ * @param {Number} digits — число знаков после запятой
+ * @return {float} — случайное число с плавающей запятой
+ */
+ function getRandomPositiveFloat (a, b, digits = 1) {
+
+  const min = Math.min(Math.abs(a), Math.abs(b));
+  const max = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = Math.random() * (max - min) + min;
+  return +result.toFixed(digits);
+ }
+
+getRandomPositiveFloat(2, 5, 2);
+
+
+
+/**
+Структура каждого объекта должна быть следующей:
+
+offer, объект — содержит информацию об объявлении. Состоит из полей:
+address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
+features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
+description, строка — описание помещения. Придумайте самостоятельно
+photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
+location, объект — местоположение в виде географических координат. Состоит из двух полей:
+lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
+lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
+ */
+
+
+const TITLES = [
+  'Райское место',
+  'Уютный уголок',
+  'Прекрасное жилье',
+  'Счастье путешественника'
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECK_IN_OUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+/**
+ * Функция для выбора случайного элемента из массива
+ * @param {Array} elements — массив
+ * @return {} — случайный элемент
+ */
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+
+const createOffer = () => {
+
+  let avatarNumber = getRandomPositiveInteger(1, 10);
+  if (avatarNumber < 10) {
+    avatarNumber = '0' + avatarNumber;
   }
 
-  const coordinate = Math.random() * (max - min + 1) + min;
-  return coordinate.toFixed(decimals);
-}
+  return {
+    author: {
+      avatar: 'img/avatars/user' + avatarNumber + '.png'
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      adress: '',
+      price: getRandomPositiveInteger(0, 200000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveInteger(1, 10),
+      guests: getRandomPositiveInteger(1, 40),
+      checkin: getRandomArrayElement(CHECK_IN_OUT),
+      checkout: getRandomArrayElement(CHECK_IN_OUT),
+    }
+  };
+};
 
-getRandomCoordinate(2, 5, 2);
-
-function getRandomCoordinate2 (min, max, decimals) {
-  if ((min >= max) || (min < 0)) {
-    return 'Диапазон для координаты задан неверно';
-  }
-
-  let coordinate;
-
-  if ((max - min) < 1) {
-    coordinate = Math.random() * (max - min) + min;
-  } else {
-    coordinate = Math.random() * (max - min + 1) + min;
-  }
-  return coordinate.toFixed(decimals);
-}
-
-getRandomCoordinate2(2, 5, 2);
+console.log(
+  createOffer()
+);
